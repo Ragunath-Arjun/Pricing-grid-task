fd53c2a096cbeed2f27db48a502a47a6b1ae2fea
ragunath arjun <ragunath219@gmail.com>
2022-11-29 02:42:35 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,99 +1,169 @@
 import logo from './logo.svg';
 import './App.css';
+import Pricinggrid from './Pricinggrid';
 
 function App() {
+  let details=[
+    {
+      name:"FREE",
+      plan:0,
+      plandetails:[
+        {
+          title:"Single User",
+          fonticon:true,
+
+
+        },
+        {
+          title:"5GB Storage",
+          fonticon:true,
+
+
+        },
+        {
+          title:"Unlimited Public Projects",
+          fonticon:true,
+
+
+          
+        },
+        {
+          title:"Community Access",
+          fonticon:true,
+
+
+        },
+        {
+          title:"Unlimited Private Projects",
+          tick:true,
+
+        },
+        {
+          title:"Dedicated Phone Support",
+          tick:true,
+
+        },
+        {
+          title:"Free Subdomain",
+          tick:true,
+
+        },
+        {
+          title:"Monthly Status Reports",
+          tick:true,
+
+        }
+      ]
+    },
+    {
+      name:"PLUS",
+      plan:9,
+      plandetails:[
+        {
+          title:"5 Users",
+          fonticon:true,
+          fontwei:true,
+
+        },
+        {
+          title:"50GB Storage",
+          fonticon:true,
+
+        },
+        {
+          title:"Unlimited Public Projects",
+          fonticon:true,
+
+        },
+        {
+          title:"Community Access",
+          fonticon:true,
+
+        },
+        {
+          title:"Unlimited Private Projects",
+          fonticon:true,
+
+        },
+        {
+          title:"Dedicated Phone Support",
+          fonticon:true,
+
+        },
+        {
+          title:"Free Subdomain",
+          fonticon:true,
+
+        },
+        {
+          title:"Monthly Status Reports",
+          tick:true
+        }
+      ]
+    },
+    {
+      name:"PRO",
+      plan:49,
+      plandetails:[
+        {
+          title:"Unlimited Users",
+          fonticon:true,
+          fontwei:true,
+
+        },
+        {
+          title:"150GB Storage",
+          fonticon:true,
+
+        },
+        {
+          title:"Unlimited Public Projects",
+          fonticon:true,
+
+        },
+        {
+          title:"Community Access",
+          fonticon:true,
+
+        },
+        {
+          title:"Unlimited Private Projects",
+          fonticon:true,
+
+        },
+        {
+          title:"Dedicated Phone Support",
+          fonticon:true,
+
+        },
+        {
+          title:"Free Subdomain",
+          fonticon:true,
+
+        },
+        {
+          title:"Monthly Status Reports",
+          fonticon:true
+        }
+      ]
+    }
+
+]
+// console.log(details.plandetails.title);
+
   return (
-    <Pricinggrid1 />
-  )
-}
-
-function Pricinggrid1(){
-  return(
-    <section className="pricing">
-    <div className='container'>
-      <div className='row'>
-        <div className='col-lg-4 col-md-6 col-sm-12 col-xl-4'>
-          <div className="pg-1 card">
-            <div className="pg-1-about">
-              <h4 className="text-center text-muted fs">FREE</h4>
-              <h1 className="text-center">$0<span className="month">/month</span></h1>
-              <hr></hr>
-              <ul class="fa-ul">
-              <li><span class="fa-li"><i class="fa fa-check"></i></span>Single User</li>
-              <li><span class="fa-li"><i class="fa fa-check"></i></span>5GB Storage</li>
-              <li><span class="fa-li"><i class="fa fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span class="fa-li"><i class="fa fa-check"></i></span>Community Access</li>
-              <li class="text-muted"><span class="fa-li"><i class="fa fa-times"></i></span>Unlimited
-                Private Projects</li>
-              <li class="text-muted"><span class="fa-li"><i class="fa fa-times"></i></span>Dedicated
-                Phone Support</li>
-              <li class="text-muted"><span class="fa-li"><i class="fa fa-times"></i></span>Free Subdomain
-              </li>
-              <li class="text-muted"><span class="fa-li"><i class="fa fa-times"></i></span>Monthly Status
-                Reports</li>
-            </ul>
-            <div class="d-grid">
-              <a href="#" class="btn btn-primary text-uppercase">Button</a>
-            </div>
-            </div>
-        </div>
-        </div>
-
-        <div className="col-lg-4 col-md-6 col-sm-12 col-xl-4">
-            <div className="pg-2">
-            <div className="pg-2-about">
-              <h4 className="text-center text-muted fs">PLUS</h4>
-              <h1 className="text-center">$9<span className="month">/month</span></h1>
-              <hr></hr>
-              <ul class="fa-ul">
-              <li><span class="fa-li"><i class="fa fa-check"></i></span><strong>5 Users</strong></li>
-              <li><span class="fa-li"><i class="fa fa-check"></i></span>50GB Storage</li>
-              <li><span class="fa-li"><i class="fa fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span class="fa-li"><i class="fa fa-check"></i></span>Community Access</li>
-              <li><span class="fa-li"><i class="fa fa-check"></i></span>Unlimited Private Projects</li>
-              <li><span class="fa-li"><i class="fa fa-check"></i></span>Dedicated Phone Support</li>
-              <li><span class="fa-li"><i class="fa fa-check"></i></span>Free Subdomain</li>
-              <li class="text-muted"><span class="fa-li"><i class="fa fa-times"></i></span>Monthly Status
-                Reports</li>
-            </ul>
-            <div class="d-grid">
-              <a href="#" class="btn btn-primary text-uppercase">Button</a>
-            </div>
-            </div>
-            </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6 col-sm-12 col-xl-4">
-            <div className="pg-3">
-            <div className="pg-3-about">
-              <h4 className="text-center text-muted fs">PRO</h4>
-              <h1 className="text-center">$49<span className="month">/month</span></h1>
-              <hr></hr>
-              <ul class="fa-ul">
-              <li><span class="fa-li"><i class="fa fa-check"></i></span><strong>Unlimited Users</strong>
-              </li>
-              <li><span class="fa-li"><i class="fa fa-check"></i></span>150GB Storage</li>
-              <li><span class="fa-li"><i class="fa fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span class="fa-li"><i class="fa fa-check"></i></span>Community Access</li>
-              <li><span class="fa-li"><i class="fa fa-check"></i></span>Unlimited Private Projects</li>
-              <li><span class="fa-li"><i class="fa fa-check"></i></span>Dedicated Phone Support</li>
-              <li><span class="fa-li"><i class="fa fa-check"></i></span><strong>Unlimited</strong> Free
-                Subdomains</li>
-              <li><span class="fa-li"><i class="fa fa-check"></i></span>Monthly Status Reports</li>
-            </ul>
-            <div class="d-grid">
-              <a href="#" class="btn btn-primary text-uppercase">Button</a>
-            </div>
-
-
-            </div>
-
-         
-      </div>
+  <>
+  <div className='container'>
+    <div className='row mt-5'>
+      {
+        details.map((card)=>{
+        return <Pricinggrid card={card}/>
+        })
+        
+        }
     </div>
-    </div>
-    </div>
-    </section>
+  </div>
+    </>
   )
 }
 
---
+++ src/Pricinggrid.js
@@ -0,0 +1,29 @@
+export default function Pricinggrid({ card }) {
+  return (
+    <div class="col-lg-4 pricing">
+      <div class="card mb-5 mb-lg-0 pricing1">
+        <div class="card-body">
+          <h5 class="card-title text-muted text-uppercase text-center">
+            {card.name}
+          </h5>
+          <h6 class="card-price text-center" style={{fontSize:"3rem",fontWeight:"bold"}}>
+            ${card.plan}
+            <span class="period" style={{fontSize:"1rem"}}>/month</span>
+          </h6>
+          <hr/>
+          <ul class="fa-ul">
+            {card.plandetails.map((item) => {
+              return <li class={`${item.tick ? 'text-muted ' : 'a' } ${item.fontwei ? "font-weight-bold" : ""}`}><span><i class={`${item.fonticon ? "fa fa-check" : "fa fa-times"}`}></i></span>
+              {item.title}</li>;
+            }) }
+          </ul>
+          <div class="d-grid">
+            <a href="#" class="btn btn-primary text-uppercase">
+              Button
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
