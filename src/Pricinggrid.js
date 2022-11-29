8b4d1b783b1a0ec66710111ad256007682cf36f5
ragunath arjun <ragunath219@gmail.com>
2022-11-29 19:39:28 +0530

    Final Update

--- src/Pricinggrid.js
+++ src/Pricinggrid.js
@@ -3,10 +3,10 @@ export default function Pricinggrid({ card }) {
     <div class="col-lg-4 pricing">
       <div class="card mb-5 mb-lg-0 pricing1">
         <div class="card-body">
-          <h5 class="card-title text-muted text-uppercase text-center">
+          <h5 class="card-title text-muted text-uppercase text-center" style={{opacity:"1"}}>
             {card.name}
           </h5>
-          <h6 class="card-price text-center" style={{fontSize:"3rem",fontWeight:"bold"}}>
+          <h6 class="card-price text-center" style={{fontSize:"2.6rem",fontWeight:"bold"}}>
             ${card.plan}
             <span class="period" style={{fontSize:"1rem"}}>/month</span>
           </h6>
